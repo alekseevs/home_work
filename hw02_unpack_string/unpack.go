79d9e18dd181f35a63cc94732407231ffcfa7274
Sergey Alekseev <salekseev@quantumbrains.com>
2020-04-11 21:12:19 +0300

    hw2 compl

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,67 @@ package hw02_unpack_string //nolint:golint,stylecheck
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"os"
+	"strings"
+	"regexp"
 )
 
 var ErrInvalidString = errors.New("invalid string")
+var LettersCountRegexp = regexp.MustCompile(`(?P<letter>[\p{L}])(?P<count>[0-9]*)?`)
+const MaxLettersCount int = 9
 
-func Unpack(_ string) (string, error) {
-	// Place your code here
-	return "", nil
+
+func Unpack(s string) (string, error) {
+	if len(s) == 0 {
+		return "", nil
+	}
+
+	if !LettersCountRegexp.MatchString(s) {
+		return "", ErrInvalidString
+	}
+
+	matches := LettersCountRegexp.FindAllStringSubmatch(s, -1)
+	var builder strings.Builder
+
+	for _, match := range matches {
+		matchedLetter, rawCount := match[1], match[2]
+
+		count, err := strconv.Atoi(rawCount)
+		if err != nil && len(rawCount) == 0 {
+			count = 1
+		}
+
+		if count == 0 || count > MaxLettersCount {
+			return "", ErrInvalidString
+		}
+
+		repeatedString := strings.Repeat(matchedLetter, count)
+		builder.WriteString(repeatedString)
+	}
+
+	return builder.String(), nil
+}
+
+
+
+func digit_checker(symbol string) bool{
+	if _, err := strconv.Atoi(symbol); err == nil {
+		// fmt.Printf("%q looks like a number.\n", symbol)
+		return true
+	} else {
+		return false
+	}
+
+}
+
+func string_to_int_conventer(s string) int{
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		// handle error
+		fmt.Println(err)
+		os.Exit(2)
+	}
+	return i
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -48,29 +48,29 @@ func TestUnpack(t *testing.T) {
 	}
 }
 
-func TestUnpackWithEscape(t *testing.T) {
-	t.Skip() // Remove if task with asterisk completed
+// func TestUnpackWithEscape(t *testing.T) {
+// 	t.Skip() // Remove if task with asterisk completed
 
-	for _, tst := range [...]test{
-		{
-			input:    `qwe\4\5`,
-			expected: `qwe45`,
-		},
-		{
-			input:    `qwe\45`,
-			expected: `qwe44444`,
-		},
-		{
-			input:    `qwe\\5`,
-			expected: `qwe\\\\\`,
-		},
-		{
-			input:    `qwe\\\3`,
-			expected: `qwe\3`,
-		},
-	} {
-		result, err := Unpack(tst.input)
-		require.Equal(t, tst.err, err)
-		require.Equal(t, tst.expected, result)
-	}
-}
+// 	for _, tst := range [...]test{
+// 		{
+// 			input:    `qwe\4\5`,
+// 			expected: `qwe45`,
+// 		},
+// 		{
+// 			input:    `qwe\45`,
+// 			expected: `qwe44444`,
+// 		},
+// 		{
+// 			input:    `qwe\\5`,
+// 			expected: `qwe\\\\\`,
+// 		},
+// 		{
+// 			input:    `qwe\\\3`,
+// 			expected: `qwe\3`,
+// 		},
+// 	} {
+// 		result, err := Unpack(tst.input)
+// 		require.Equal(t, tst.err, err)
+// 		require.Equal(t, tst.expected, result)
+// 	}
+// }
