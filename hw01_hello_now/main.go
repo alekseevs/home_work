2a840ff4a403af1d17fbd54d9f8ddf9e8c3cce29
Sergey Alekseev <salekseev@quantumbrains.com>
2020-04-11 19:54:51 +0300

    hw complite

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"github.com/beevik/ntp"
+	"time"
+	"os"
+)
+// current time: <время>
+// exact time: <время>
+// (без учёта монотонного времени (без "m=±<value>" в выводе))
+
+func formatTime(t time.Time) string {
+	return t.Format("2006-01-02 15:04:05 -0700 UTC")
+}
+
 func main() {
-	// Place your code here
+	exactTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	localTime := time.Now()
+
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("current time: %s\n", formatTime(localTime))
+	fmt.Printf("exact time: %s\n", formatTime(exactTime))
 }
